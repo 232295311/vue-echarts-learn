97e38cbc5952b1d98a4ba626f3fcaafecaa45f95
232295311 <232295311@qq.com>
2021-04-29 17:40:12 +0800

    第一次提交，初始化项目，引入echarts和axios。

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import axios from 'axios'
+// 请求基准路径的配置
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/'
+// 将axios挂载到vue的原型对象上
+// this.$http即可获得axios对象
+Vue.prototype.$http = axios
+
+// 将全局的echarts对象挂载到vue的原型对象上
+// this.$echarts即可获得实例对象
+Vue.prototype.$echarts = window.echarts
+
 Vue.config.productionTip = false
 
 new Vue({
