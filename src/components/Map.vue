5a7006c06abe14589bdfd262f29cd68dcdbe7b7c
232295311 <232295311@qq.com>
2021-05-04 11:17:08 +0800

    新增中国地图组件

---
+++ src/components/Map.vue
@@ -0,0 +1,151 @@
+<!-- 商家分布图表 -->
+<template>
+  <div class='com-container'
+       @dblclick="revertMap">
+
+    <div class='com-chart'
+         ref='map_ref'></div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { getProvinceMapInfo } from '@/utils/map_utils'
+export default {
+  data() {
+    return {
+      chartInstance: null,
+      allData: null,
+      mapData: {} // 所获取的省份的地图矢量数据 用作缓存
+    }
+  },
+  mounted() {
+    this.initChart()
+    this.getData()
+    window.addEventListener('resize', this.screenAdapter)
+    this.screenAdapter()
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.screenAdapter)
+  },
+  methods: {
+    async initChart() {
+      this.chartInstance = this.$echarts.init(this.$refs.map_ref, 'chalk')
+      //   获取中国地图的矢量数据
+      //   http://localhost:9999/static/map/china.json
+      //   由于我们现在获取的地图矢量数据并不是位于KOA2的后台
+      const ret = await axios.get('http://localhost:8999/static/map/china.json')
+      this.$echarts.registerMap('china', ret.data)
+
+      const initOption = {
+        title: {
+          text: '▍商家分布',
+          left: 20,
+          top: 20
+        },
+        geo: {
+          type: 'map',
+          map: 'china',
+          top: '5%',
+          bottom: '5%',
+          itemStyle: {
+            areaColor: '#2E72BF',
+            borderColor: '#333'
+          }
+        },
+        legend: {
+          left: '5%',
+          bottom: '5%',
+          orient: 'vertical'
+        }
+      }
+      this.chartInstance.setOption(initOption)
+      this.chartInstance.on('click', async (arg) => {
+          console.log(arg)
+        //   arg.name得到所点击的省份，这个省份他是中文
+        const provinceInfo = getProvinceMapInfo(arg.name)
+        // 判断当前所点击的这个省份的数据是否存在
+        if (!this.mapData[provinceInfo.key]) {
+          const ret = await axios.get(
+            'http://localhost:8999' + provinceInfo.path
+          )
+          this.mapData[provinceInfo.key] = ret.data
+          this.$echarts.registerMap(provinceInfo.key, ret.data)
+        }
+
+        const changeOption = {
+          geo: {
+            map: provinceInfo.key
+          }
+        }
+        this.chartInstance.setOption(changeOption)
+      })
+    },
+    async getData() {
+      const { data: ret } = await this.$http.get('map')
+      this.allData = ret
+      this.updateChart()
+    },
+    updateChart() {
+      // 处理图表需要的数据
+      const legendArr = this.allData.map((item) => {
+        return item.name
+      })
+      const seriesArr = this.allData.map((item) => {
+        // return的对象代表的是一个类别下的所有散点数据
+        // 如果想在地图中显示散点的数据，所以我们需要给散点的图表增加一个配置
+        return {
+          type: 'effectScatter',
+          rippleEffect: {
+            scale: 5,
+            brushType: 'stroke'
+          },
+          name: item.name,
+          data: item.children,
+          coordinateSystem: 'geo'
+        }
+      })
+      // 图例的数据
+      const dataOption = {
+        series: seriesArr,
+        legend: {
+          data: legendArr
+        }
+      }
+      this.chartInstance.setOption(dataOption)
+    },
+    screenAdapter() {
+      const titleFontSize = (this.$refs.map_ref.offsetWidth / 100) * 3.6
+      const adapterOption = {
+        title: {
+          textStyle: {
+            fontSize: titleFontSize
+          }
+        },
+        legend: {
+          itemWidth: titleFontSize / 2,
+          itemHeight: titleFontSize / 2,
+          itemGap: titleFontSize / 2,
+          textStyle: {
+            fontSize: titleFontSize / 2
+          }
+        }
+      }
+      this.chartInstance.setOption(adapterOption)
+      this.chartInstance.resize()
+    },
+    // 回到中国地图
+    revertMap() {
+      const revertOption = {
+        geo: {
+          map: 'china'
+        }
+      }
+      this.chartInstance.setOption(revertOption)
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+</style>
